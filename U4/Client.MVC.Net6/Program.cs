c694070510983632e4c8be0963cb774656cc0329
Arve Hansen <arveh4@outlook.com>
2023-02-08 07:31:42 +0100

    Add authentication

--- U4/Client.MVC.Net6/Program.cs
+++ U4/Client.MVC.Net6/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.VisualBasic;
+
 namespace Client.MVC.Net6
 {
     public class Program
@@ -5,6 +11,48 @@ namespace Client.MVC.Net6
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+            builder.Services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = builder.Configuration.GetValue<string>("Auth:Authority");
+
+                    options.ClientId = builder.Configuration.GetValue<string>("Auth:Client");
+                    options.ClientSecret = builder.Configuration.GetValue<string>("Auth:ClientSecret");
+                    options.ResponseType = "code";
+
+                    // 
+                    options.Scope.Clear();
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+
+                    // Save the tokens in a cookie
+                    options.SaveTokens = true;
+
+                    // We use the query parameter "acr_values" to set tenant information
+                    options.Events.OnRedirectToIdentityProvider = context =>
+                    {
+                        if (context.ProtocolMessage.RequestType == OpenIdConnectRequestType.Authentication)
+                        {
+                            var tenant = builder.Configuration.GetValue<string>("Auth:Tenant");
+                            var idp = builder.Configuration.GetValue<string>("Auth:Idp");
+                            context.ProtocolMessage.AcrValues =
+                                $"tenant:{tenant}";
+                            if (!string.IsNullOrWhiteSpace(idp))
+                            {
+                                context.ProtocolMessage.AcrValues += $" loginidp:{idp}";
+                            }
+                        }
+                        return Task.CompletedTask;
+                    };
+                });
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -24,11 +72,13 @@ namespace Client.MVC.Net6
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .RequireAuthorization(); // Disable anonymous access for the entire application
 
             app.Run();
         }
