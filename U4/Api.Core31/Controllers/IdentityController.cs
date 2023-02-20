70e817e04e5c804308eba526c1012641bf72ddab
Arve Hansen <arve.hansen@unit4.com>
2023-02-20 11:02:34 +0100

    Added api.core31 project

---
+++ U4/Api.Core31/Controllers/IdentityController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System.Linq;
+
+namespace Api.Core31.Controllers
+{
+    [Route("identity")]
+    public class IdentityController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public IdentityController(ILogger logger)
+        {
+            _logger = logger.ForContext<IdentityController>();
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var claims = User.Claims.Select(c => new { c.Type, c.Value });
+            _logger.Information("claims: {@claims}", claims);
+
+            return new JsonResult(claims);
+        }
+    }
+}
