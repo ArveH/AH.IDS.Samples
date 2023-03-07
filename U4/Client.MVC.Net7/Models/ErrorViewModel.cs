78b30f4266cd67fcb17d7270716b3c6d2fe5ac44
Arve Hansen <arve.hansen@unit4.com>
2023-03-07 13:53:39 +0100

    Created correct Client Template project

---
+++ U4/Client.MVC.Net7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Client.MVC.Net7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
